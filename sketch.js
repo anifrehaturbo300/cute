440dfe0e6e58fb3f447c6ed50768fa4aee4cfe64
anifrehaturbo300 <133776664+anifrehaturbo300@users.noreply.github.com>
2023-05-16 15:59:52 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+function setup() {
+  createCanvas(600, 600);
+    background(255);
+}
+
+function draw() {
+
+  stroke("rgba(0,0,0,0)");
+  fill("#dceff6");
+  stroke("#ffffff");
+  beginShape();
+  vertex(199.32, 270.56);
+  vertex(235.76, 221.5);
+  vertex(267.16, 221.5);
+  vertex(249.33, 270.56);
+  vertex(199.32, 270.56);
+  endShape();
+  fill("#b7dfee");
+  stroke("#ffffff");
+  beginShape();
+  vertex(267.16, 221.5);
+  vertex(333.22, 221.5);
+  vertex(351.4, 270.56);
+  vertex(249.33, 270.56);
+  vertex(267.16, 221.5);
+  endShape();
+  fill("#93d0e6");
+  stroke("#ffffff");
+  beginShape();
+  vertex(333.22, 221.5);
+  vertex(365.03, 221.5);
+  vertex(400.68, 270.56);
+  vertex(351.4, 270.56);
+  vertex(333.22, 221.5);
+  endShape();
+  fill("#dceff6");
+  stroke("#ffffff");
+  beginShape();
+  vertex(199.32, 270.56);
+  vertex(249.33, 270.56);
+  vertex(300.24, 378.5);
+  vertex(199.32, 270.56);
+  endShape();
+  fill("#b7dfee");
+  stroke("#ffffff");
+  beginShape();
+  vertex(249.33, 270.56);
+  vertex(351.4, 270.56);
+  vertex(300.24, 378.5);
+  vertex(249.33, 270.56);
+  endShape();
+  fill("#93d0e6");
+  stroke("#ffffff");
+  beginShape();
+  vertex(351.4, 270.56);
+  vertex(400.68, 270.56);
+  vertex(300.24, 378.5);
+  vertex(351.4, 270.56);
+  endShape();
+  fill(255)
+  stroke(183,223,238)
+  ellipse(mouseX,mouseY,100,100)
+}
